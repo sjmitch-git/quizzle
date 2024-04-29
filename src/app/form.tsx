2abd00d381affb88203d89a4277448d6c8564255
sjmitch-git <sjmitch20@outlook.com>
2024-04-29 19:02:51 +0100

    Feat: adds progress indicator to questions

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -1,78 +1,99 @@
 'use client'
 
-import { useContext, useState } from "react";
+import { useContext, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import { TriviaContext } from '@/contexts/TriviaContext'
-import Categories from "@/data/categories.json";
+import Categories from '@/data/categories.json'
 
-import {OPTIONS_DIFFICULTY} from '@/utils/constants'
-
+import { OPTIONS_DIFFICULTY } from '@/utils/constants'
 
 export default function Form() {
-	const triviaContext = useContext(TriviaContext)!;
-	const { updateTriviaParams, triviaParams } = triviaContext;
+	const triviaContext = useContext(TriviaContext)!
+	const { updateTriviaParams, triviaParams, updateTriviaQuestions } = triviaContext
 
-    const [category, setCategory] = useState(triviaParams.category);
-    const [categoryName, setCategoryName] = useState(triviaParams.categoryName);
-    const [difficulty, setDifficulty] = useState(triviaParams.difficulty);
+	const [category, setCategory] = useState(triviaParams.category)
+	const [categoryName, setCategoryName] = useState(triviaParams.categoryName)
+	const [difficulty, setDifficulty] = useState(triviaParams.difficulty)
 
-    const router = useRouter()
+	const router = useRouter()
 
-    const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const categoryId = e.target.value
-        const selectedCategory = Categories.find(category => category.id === Number(categoryId));
-        setCategory(categoryId)
-        if (selectedCategory) setCategoryName(selectedCategory.name)
+	const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		const categoryId = e.target.value
+		const selectedCategory = Categories.find((category) => category.id === Number(categoryId))
+		setCategory(categoryId)
+		if (selectedCategory) setCategoryName(selectedCategory.name)
 	}
 
-    const handleDifficultyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		 setDifficulty(e.target.value)
+	const handleDifficultyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setDifficulty(e.target.value)
 	}
 
-    const handleStartQuiz = () => {
-        updateTriviaParams?.({
-            category, difficulty,
-            categoryName: categoryName
-        });
-        router.push(`/quiz?category=${category}&difficulty=${difficulty}`);
-    };
+	const handleStartQuiz = () => {
+		updateTriviaQuestions([])
+		updateTriviaParams?.({
+			category,
+			difficulty,
+			categoryName: categoryName,
+		})
+		router.push(`/quiz?category=${category}&difficulty=${difficulty}`)
+	}
 
 	return (
 		<form>
-            <label className="mb-8 block">
-                <span className='sr-only'>Category:</span>
-                <select value={category} onChange={handleCategoryChange}>
-                    {Categories.map((category) => (
-                    <option key={category.id} value={category.id}>
-                        {category.name}
-                    </option>
-                    ))}
-                </select>
-            </label>
+			<label className='mb-8 block'>
+				<span className='sr-only'>Category:</span>
+				<select
+					value={category}
+					onChange={handleCategoryChange}
+					name='category'
+				>
+					{Categories.map((category) => (
+						<option
+							key={category.id}
+							value={category.id}
+						>
+							{category.name}
+						</option>
+					))}
+				</select>
+			</label>
 
-        <div className="mb-8 flex justify-center">
-            {OPTIONS_DIFFICULTY.map((item: string, index: any) => (
-                <div key={index} className={`flex relative overflow-clip first:rounded-s-md last:rounded-e-md hover:border-primary ${item === difficulty ? 'bg-info text-light' : 'bg-gray-300'}`}>
-                    <input
-                        type="radio"
-                        id={item}
-                        name="difficulty"
-                        value={item}
-                        checked={item === difficulty}
-                        onChange={handleDifficultyChange}
-                        className='absolute left-[-50px]'
-                    />
-                    <label htmlFor={item} className="w-full p-4 capitalize text-xl" tabIndex={0}>{item}</label>
-                </div>
-            ))}
-        </div>
+			<div className='mb-8 flex justify-center'>
+				{OPTIONS_DIFFICULTY.map((item: string, index: any) => (
+					<div
+						key={index}
+						className={`relative flex overflow-clip first:rounded-s-md last:rounded-e-md hover:border-primary ${item === difficulty ? 'bg-info text-light' : 'bg-gray-300'}`}
+					>
+						<input
+							type='radio'
+							id={item}
+							name='difficulty'
+							value={item}
+							checked={item === difficulty}
+							onChange={handleDifficultyChange}
+							className='absolute left-[-50px]'
+						/>
+						<label
+							htmlFor={item}
+							className='w-full p-4 text-xl capitalize'
+							tabIndex={0}
+						>
+							{item}
+						</label>
+					</div>
+				))}
+			</div>
 
-        <div className="flex justify-center">
-            <button type='button' className="btn bg-primary text-light text-xl" onClick={handleStartQuiz}>
-                Start Quiz
-            </button>
-        </div>
-      </form>
+			<div className='flex justify-center'>
+				<button
+					type='button'
+					className='btn bg-primary text-xl text-light'
+					onClick={handleStartQuiz}
+				>
+					Start Quiz
+				</button>
+			</div>
+		</form>
 	)
 }
--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,26 +2,32 @@ import { Heading } from '@/components'
 import Questions from './questions'
 import { DEFAULT_CATEGORY, DEFAULT_DIFFICULTY } from '@/utils/constants'
 import FetchData from '@/utils/fetchData'
-import {shuffleArray} from '@/utils/shuffleArray'
-import {ExtendedQuizQuestion} from '@/types/quizQuestion'
+import { shuffleArray } from '@/utils/shuffleArray'
+import { ExtendedQuizQuestion } from '@/types/quizQuestion'
 
-export default async function Quiz({ searchParams }: { searchParams: { category: string | null, difficulty: string | null } }) {
-    const category = searchParams.category || DEFAULT_CATEGORY
-    const difficulty = searchParams.difficulty || DEFAULT_DIFFICULTY
+export default async function Quiz({
+	searchParams,
+}: {
+	searchParams: { category: string | null; difficulty: string | null }
+}) {
+	const category = searchParams.category || DEFAULT_CATEGORY
+	const difficulty = searchParams.difficulty || DEFAULT_DIFFICULTY
 
-    const data = await FetchData(`category=${category}&difficulty=${difficulty}`)
+	const data = await FetchData(`category=${category}&difficulty=${difficulty}`)
 
-    const extendedData: ExtendedQuizQuestion[] = data.results.map((question: ExtendedQuizQuestion) => ({
-        ...question,
-        answers: shuffleArray([...question.incorrect_answers, question.correct_answer]),
-        answered: null,
-        value: null,
-      })).sort();
+	const extendedData: ExtendedQuizQuestion[] = data.results
+		.map((question: ExtendedQuizQuestion) => ({
+			...question,
+			answers: shuffleArray([...question.incorrect_answers, question.correct_answer]),
+			answered: null,
+			value: null,
+		}))
+		.sort()
 
 	return (
 		<div>
-            <Heading label='Quiz Questions' />
-            <Questions data={ extendedData}  />
+			<Heading label='Quiz Questions' />
+			<Questions data={extendedData} />
 		</div>
 	)
 }
